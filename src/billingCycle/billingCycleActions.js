855cc0620c29a9b043d4b097a9902131a5107771
Ricardo Matta <ricardommps@gmail.com>
2018-07-11 08:44:19 -0300

    deploy

--- src/billingCycle/billingCycleActions.js
+++ src/billingCycle/billingCycleActions.js
@@ -2,12 +2,12 @@ import axios from 'axios'
 import { toastr } from 'react-redux-toastr'
 import { reset as resetForm, initialize } from 'redux-form'
 import { showTabs, selectTab } from '../common/tab/tabActions'
+import consts from '../consts'
 
-const BASE_URL = 'http://localhost:3003/api'
 const INITIAL_VALUES = {credits:[{}], debts:[{}]}
 
 export function getList(){
-    const request = axios.get(`${BASE_URL}/billingCycles`)
+    const request = axios.get(`${consts.API_URL}/billingCycles`)
     return {
         type: 'BILLING_CYCLES_FETCHED',
         payload: request
@@ -29,7 +29,7 @@ export function remove(values){
 function submit(values, method){
     return dispatch => {
         const id = values._id ? values._id : ''
-        axios[method](`${BASE_URL}/billingCycles/${id}`,values)
+        axios[method](`${consts.API_URL}/billingCycles/${id}`,values)
         .then(resp => {
             toastr.success('Sucesso', 'Operação Realizada com sucesso.')
             dispatch(init())
